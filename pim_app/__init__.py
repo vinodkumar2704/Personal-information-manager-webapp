ea806072443a021db3ce30bbd60607583826f916
vinodkumar2704 <vinodkb2704@gmail.com>
2021-07-30 22:04:14 +0530

    stopped deploy:(

--- pim_app/__init__.py
+++ pim_app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     
     
     app.config.from_mapping(
-        DATABASE = "postgres://wgklquffwkngqd:28bbbd590fd7a474b644e1eb03bd28e3e6ffedc41b61faa779e5aec27f29a631@ec2-54-145-185-178.compute-1.amazonaws.com:5432/daiss07i4ejvib"
+        DATABASE = "pim"
     )
     
     from . import notes
